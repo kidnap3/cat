17c69611e91ed6b5949009e1c6b72502b7fd2943
kidnap3 <fagjaar@gmail.com>
2022-07-25 17:52:19 +0700

    harusnya ada

--- hacktiv8/Code/Math.floor.js
+++ hacktiv8/Code/Math.floor.js
@@ -3,3 +3,6 @@ console.log(Math.floor(Math.random(100)*100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
 console.log(Math.floor(124))
+
+
+sofjasijdjfalsdk HARUSNYA BISAAAAAAA
